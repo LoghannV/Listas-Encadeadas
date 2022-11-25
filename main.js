1a9219d6a5566eaaab45bc8681e210c0f894ce71
LoghannV <loghann2000@gmail.com>
2022-11-25 15:52:35 -0300

    Aula 6

--- main.js
+++ main.js
@@ -59,12 +59,43 @@ class Lista{
         }
         return false
     }
+    isEmpty(){
+        if(this.count==0){
+            return true
+        }else{
+            return false
+        }
+    }
+    size(){
+        return this.count
+    }
+    indexOf(element){
+        let posicao
+        let current = element
+        let Elementos = this.getElementAt(0)
+        while(current!=Elementos.value && posicao<this.count){
+            posicao++
+            Elementos = Elementos.next
+        }
+        return posicao
+    }
+    removeAt(position){
+        if(position>=0 && position <=this.count){
+            const current = this.head
+            if(position == 0){
+                this.head = current.next
+            }else{
+                const previous = this.getElementAt(position - 1)
+                const current = previous.next
+                previous.next = current.next
+            }
+        }
+        this.count --
+    }
     //Demais métodos da classe Lista
     //remove() remove um elemento da lista
     //indexOf(element) retornar o indice do elemento da lista 
     //removeAt(position) remover o elemento de uma posicao especifica da lista 
-    //isEmpty() retorna se lista esta vazia 
-    //size() retorna o tamanho da lista 
 }
 
 const nova_lista = new Lista()
@@ -76,4 +107,5 @@ nova_lista.push(5)
 nova_lista.insertAt(17,2)
 
 //document.getElementById("view").innerHTML = JSON.stringify(nova_lista,null,2)
-console.log(JSON.stringify(nova_lista,null,2))
+//console.log(JSON.stringify(nova_lista,null,2))
+console.log(nova_lista.indexOf(2))
