c623cbeef04695a0174f240670f948ea122ad6f0
LoghannV <loghann2000@gmail.com>
2022-11-25 15:04:04 -0300

    Aula 2

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
+
 class Node{
     constructor(element){
         this.value = element;
         this.next = undefined;
     }
+}
+
+class Lista{
+    constructor(){
+        this.head = undefined;
+        this.count = 0;
+    }
+    //Demais métodos da classe Lista
+    //push() adicionar um elemento no fim da lista
+    //inserAt(element,position) add um elemento em qualquer posicao da lista
+    //remove() remove um elemento da lista
+    //getElementAt(position) retornar o elemento da posicao especifica da lista 
+    //indexOf(element) retornar o indice do elemento da lista 
+    //removeAt(position) remover o elemento de uma posicao especifica da lista 
+    //isEmpty() retorna se lista esta vazia 
+    //size() retorna o tamanho da lista 
 }
